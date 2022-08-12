f9e42dad84a3fe3452f626328304776fd227fadd
Luke <cutluk1@gmail.com>
2022-08-12 14:59:30 -0400

    Initial Commit

---
+++ Solitaire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Solitaire
+//
+//  Created by Luke Cutting on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Solitaire/SolitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolitaireApp.swift
+//  Solitaire
+//
+//  Created by Luke Cutting on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
